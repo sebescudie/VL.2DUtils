a67353cc2a96dc44bc3f9eda8766df5473d22b3a
sebescudie <seb.espm@gmail.com>
2019-09-09 09:51:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# VL.2DUtils
+
+_A collection of tools for 2D purposes._
+
+## What's inside
+
+- *PolygonContainsPoint :* tells whether a polygon contains a point
---
+++ src/2D.Utils/Utils.cs
@@ -0,0 +1,30 @@
+﻿using Xenko.Core.Mathematics;
+using System.Collections.Generic;
+
+namespace _2D.Geometry
+{
+    public class Utils
+    {
+        public static bool PolygonContainsPoint(Vector2 point, List<Vector2> polygon)
+        {
+            int numPolygons = polygon.Count;
+            bool isInsidePolygon = false;
+
+            var i = 0;
+            int j = numPolygons - 1;
+
+            for (; i < numPolygons; j = i++)
+            {
+                if (((polygon[i].Y > point.Y) !=
+                    (polygon[j].Y > point.Y)) &&
+                    (point.X < (polygon[j].X - polygon[i].X) *
+                    (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) +
+                    polygon[i].X))
+                {
+                    isInsidePolygon = !isInsidePolygon;
+                }
+            }
+            return isInsidePolygon;
+        }
+    }
+}
